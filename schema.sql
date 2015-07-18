d59e1edcaae086cafc961bd5c050aba5e48c0484
Owens O'Brien <owensobrien@gmail.com>
2015-07-18 00:26:36 -0400

    schema update

--- schema.sql
+++ schema.sql
@@ -1,2 +1,23 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS file CASCADE;
+DROP TABLE IF EXISTS files CASCADE;
+
+
+CREATE TABLE users(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255),
+	wantmade TEXT,
+	geoloc VARCHAR(255),
+	avatar_url TEXT
+
+)
+
+CREATE TABLE files(
+	id SERIAL PRIMARY KEY,
+	url TEXT,
+	votes INTEGER,
+	chosen BOOLEAN,
+	filetype VARCHAR(255),
+	dateuploaded DATE,
+	users_id INTEGER references users,
+
+	)
